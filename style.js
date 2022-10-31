abdd7266482aa1578e695ae301cbaad58e49e6ba
Moin Hasanfatta <112717999+MoinHasanfatta@users.noreply.github.com>
2022-10-31 13:57:45 +0530

    Update style.js

--- style.js
+++ style.js
@@ -1,12 +1,5 @@
 let emojis = document.getElementById("icons");
 
-let isShow = true;
 function lukkaChuppi() {
-  if (isShow) {
-    emojis.style.display = "none";
-    isShow = false;
-  } else {
-    emojis.style.display = "block";
-    isShow = true;
-  }
-}
+    emojis.classList.toggle('hide')
+ }
